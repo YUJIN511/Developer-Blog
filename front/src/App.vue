8c2af5a54a7653d2e6a331f9d49ce1eb8ebc9a12
moo1323@naver.com <moo1323@naver.com>
2020-08-07 13:52:54 +0900

    fix UI

--- front/src/App.vue
+++ front/src/App.vue
@@ -11,13 +11,14 @@ import Header from "@/components/common/Header.vue";
 export default {
   name: "App",
   components: {
-    Header,
-  },
+    Header
+  }
 };
 </script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap");
+@import "@/assets/_variables.scss";
 #app {
   font-family: "Noto Sans KR", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,6 +26,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   overflow-x: hidden;
+  padding-bottom: 50px;
+  background-color: $bgColor;
 }
 
 button {
