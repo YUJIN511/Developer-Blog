8f59a56117b162f8069293b35a2cc578fbb05df7
kimnayu <mung0605@naver.com>
2020-08-14 01:22:46 +0900

    [F]feat: get library

--- front/src/views/Library.vue
+++ front/src/views/Library.vue
@@ -27,57 +27,68 @@ import LimitedAccess from "@/components/user/LimitedAccess.vue";
 
 import { mapMutations, mapGetters } from "vuex";
 
+import axios from "axios";
+
+const SERVER_URL = "http://i3a604.p.ssafy.io:8081";
+
 export default {
   components: {
     FlexArticles,
-    LimitedAccess,
+    LimitedAccess
   },
   data: function() {
     return {
-      articleData: [],
+      articleData: []
     };
   },
   methods: {
     ...mapMutations({
-      paintBtn: "navbarMini/paintBtn",
+      paintBtn: "navbarMini/paintBtn"
     }),
     ...mapGetters({
       getIsLogin: "user/getIsLogin",
-    }),
+      getEmail: "user/getEmail"
+    })
   },
   mounted() {
     this.paintBtn(document.querySelector("#btn-library"));
   },
   created() {
-    this.articleData = [
-      {
-        thumbUrl:
-          "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        title: "글 제목1",
-        desc:
-          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea voluptates eos laboriosam quo eaque earum laudantium modi natus, mollitia animi tempore, sint iste velit voluptatum. Est possimus rem, nostrum numquam totam natus eaque, enim sit nisi earum accusantium aliquid tenetur?",
-        profileUrl:
-          "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        iconUrl: "@/assets/tree.svg",
-        name: "닉네임1",
-        isLiked: true,
-        likeCnt: 10,
-      },
-      {
-        thumbUrl:
-          "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        title: "글 제목2",
-        desc:
-          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea voluptates eos laboriosam quo eaque earum laudantium modi natus, mollitia animi tempore, sint iste velit voluptatum. Est possimus rem, nostrum numquam totam natus eaque, enim sit nisi earum accusantium aliquid tenetur?",
-        profileUrl:
-          "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-        iconUrl: "@/assets/tree.svg",
-        name: "닉네임2",
-        isLiked: false,
-        likeCnt: 9,
-      },
-    ];
-  },
+    axios
+      .get(`${SERVER_URL}/api//post/storage?email=${this.getEmail()}`)
+      .then(res => {
+        this.articleData = res.data;
+      })
+      .catch(err => console.log(err));
+    // this.articleData = [
+    //   {
+    //     thumbUrl:
+    //       "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    //     title: "글 제목1",
+    //     desc:
+    //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea voluptates eos laboriosam quo eaque earum laudantium modi natus, mollitia animi tempore, sint iste velit voluptatum. Est possimus rem, nostrum numquam totam natus eaque, enim sit nisi earum accusantium aliquid tenetur?",
+    //     profileUrl:
+    //       "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    //     iconUrl: "@/assets/tree.svg",
+    //     name: "닉네임1",
+    //     isLiked: true,
+    //     likeCnt: 10,
+    //   },
+    //   {
+    //     thumbUrl:
+    //       "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    //     title: "글 제목2",
+    //     desc:
+    //       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea voluptates eos laboriosam quo eaque earum laudantium modi natus, mollitia animi tempore, sint iste velit voluptatum. Est possimus rem, nostrum numquam totam natus eaque, enim sit nisi earum accusantium aliquid tenetur?",
+    //     profileUrl:
+    //       "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+    //     iconUrl: "@/assets/tree.svg",
+    //     name: "닉네임2",
+    //     isLiked: false,
+    //     likeCnt: 9,
+    //   },
+    // ];
+  }
 };
 </script>
 
