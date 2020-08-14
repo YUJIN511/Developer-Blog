814502d1136ee0133b2aaca2ef10754ed81b5207
jaemin <average10@naver.com>
2020-08-15 03:13:33 +0900

    [Feat B] add to FollowList

--- back/src/main/java/com/papple/blog/repository/ProfileRepository.java
+++ back/src/main/java/com/papple/blog/repository/ProfileRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.papple.blog.payload.response.FollowList;
+
 @Mapper
 public interface ProfileRepository {
 	List<String> searchByEmail(String email);
 	int insertProfile(String email, String path);
 	int deleteProfile(String email, String path);
 	int unProfile(String email);
+	List<FollowList> myFollowList(String email);
 }
