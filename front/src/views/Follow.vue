f622439246695461750b38701c5a8b9e23795019
kimnayu <mung0605@naver.com>
2020-08-15 23:55:49 +0900

    [F]feat: get followers article

--- front/src/views/Follow.vue
+++ front/src/views/Follow.vue
@@ -31,34 +31,7 @@ export default {
   data: function() {
     return {
       data: [],
-      articleData: [
-        {
-          thumbUrl:
-            "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-          title: "글 제목1",
-          desc:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea voluptates eos laboriosam quo eaque earum laudantium modi natus, mollitia animi tempore, sint iste velit voluptatum. Est possimus rem, nostrum numquam totam natus eaque, enim sit nisi earum accusantium aliquid tenetur?",
-          profileUrl:
-            "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-          iconUrl: "@/assets/tree.svg",
-          name: "닉네임1",
-          isLiked: true,
-          likeCnt: 10
-        },
-        {
-          thumbUrl:
-            "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-          title: "글 제목2",
-          desc:
-            "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea voluptates eos laboriosam quo eaque earum laudantium modi natus, mollitia animi tempore, sint iste velit voluptatum. Est possimus rem, nostrum numquam totam natus eaque, enim sit nisi earum accusantium aliquid tenetur?",
-          profileUrl:
-            "https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-          iconUrl: "@/assets/tree.svg",
-          name: "닉네임2",
-          isLiked: false,
-          likeCnt: 9
-        }
-      ]
+      articleData: []
     };
   },
   methods: {
@@ -85,6 +58,14 @@ export default {
           console.log(this.data);
         })
         .catch(err => console.log(err));
+    },
+    async fetchArticleData() {
+      await axios
+        .get(`${SERVER_URL}/api/main/followLatest?email=${this.getEmail()}`)
+        .then(res => {
+          this.articleData = res.data;
+        })
+        .catch(err => console.log(err));
     }
   },
   mounted() {
@@ -92,6 +73,7 @@ export default {
   },
   created() {
     this.fetchFollowerData();
+    this.fetchArticleData();
   }
 };
 </script>
