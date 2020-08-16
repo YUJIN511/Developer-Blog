e99fd08f793348cb132b6cb3ef57c034b2f77e65
kimnayu <mung0605@naver.com>
2020-08-17 03:33:23 +0900

    [F}fix: emailsent modal design fix

--- front/src/components/user/EmailModal.vue
+++ front/src/components/user/EmailModal.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="container-emailsent hide">
-    <div class="background-emailsent" @click="closeModal"></div>
+  <div class="container-emailsent">
+    <div class="background-emailsent" @click="closeLogin"></div>
     <div class="modal-emailsent">
-      <h1>회원가입 완료</h1>
-      <div class="modal-body">
-        <p>회원가입이 완료되었습니다.</p>
-        <p>계정 활성화를 위한 이메일이 가입하신 이메일로 발송되었습니다.</p>
-        <p>계정을 활성화 해주세요.</p>
+      <div class="left-emailsent"></div>
+      <div class="right-emailsent">
+        <!-- <button class="btn-close" @click="closeLogin">✖</button> -->
+        <div class="title">
+          <h1>이메일 발송</h1>
+        </div>
+        <div class="modal-emailsent-body">
+          <div>회원가입이 완료 되었습니다.</div>
+          <div>
+            계정 활성화를 위한 메일이
+            <span class="span-red">가입하신 이메일주소</span>로 발송되었습니다.
+          </div>
+          <div>계정을 활성화 해주세요.</div>
+        </div>
+        <div class="emailsent-footer">
+          <a class="link-join" href="javascript:void(0)" @click="moveToJoin">계정 만들기</a>
+        </div>
       </div>
-      <!-- <button class="btn-close" @click="closeModal">✖</button> -->
     </div>
   </div>
 </template>
@@ -25,9 +36,13 @@ export default {
 </script>
 
 <style scoped lang="scss">
+/** base */
+
+$minimumWidth: 950px;
+
 .container-emailsent {
   position: fixed;
-  z-index: 5;
+  z-index: 99999;
   top: 0;
   left: 0;
   width: 100vw;
@@ -45,25 +60,87 @@ export default {
   background-color: rgba(0, 0, 0, 0.4);
 }
 
-// modal
-
 .modal-emailsent {
-  position: fixed;
-  width: 520px;
-  height: 520px;
-  background-color: rgb(240, 240, 240);
-  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-  border-radius: 3%;
   display: flex;
-  flex-direction: column;
+  position: fixed;
+  width: 400px;
+  height: 520px;
+  background-color: rgb(252, 252, 252);
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  @media (min-width: $minimumWidth) {
+    width: 780px;
+  }
+  .title {
+    text-align: left;
+    margin-bottom: 24px;
+    h1 {
+      margin-bottom: 12px;
+    }
+  }
+  .modal-emailsent-body {
+    .span-red {
+      color: red;
+    }
+    div {
+      text-align: left;
+    }
+  }
+  .left-emailsent {
+    display: none;
+    width: 45%;
+    background-image: url(https://cdn.pixabay.com/photo/2015/09/04/23/28/wordpress-923188_960_720.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    @media (min-width: $minimumWidth) {
+      display: block;
+    }
+  }
+  .right-emailsent {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 24px;
+    width: 100%;
+    @media (min-width: $minimumWidth) {
+      width: 55%;
+    }
+
+    .btn-close {
+      position: absolute;
+      right: 20px;
+      top: 10px;
+      opacity: 0.8;
+    }
+    .join-link {
+      margin-top: 10px;
+      text-align: center;
+      font-size: 0.9em;
+    }
+
+    .emailsent-footer {
+      display: flex;
+      justify-content: right;
+      justify-content: baseline;
+      // align-items: f;
+      width: 100%;
+      margin-top: 12px;
+      a:hover {
+        opacity: 0.7;
+      }
+      .link-repassword {
+        text-decoration: none;
+        color: #ff5651;
+      }
+      .link-join {
+        text-decoration: none;
+        color: dodgerblue;
+      }
+    }
+  }
 }
 
-.modal-body {
-  padding-top: 100px;
-}
-
-h1 {
-  margin: 30px 0px;
+hr {
+  margin: 25px 0px;
 }
 
 .hide {
