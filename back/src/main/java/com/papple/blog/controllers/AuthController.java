9e6431fc3a098e150384625785baacfc167d3d6d
고유진 <smile970511@gmail.com>
2020-08-19 23:18:56 +0900

    [Feat B] naver login

--- back/src/main/java/com/papple/blog/controllers/AuthController.java
+++ back/src/main/java/com/papple/blog/controllers/AuthController.java
@@ -480,10 +480,12 @@ public class AuthController {
 	
 	@GetMapping("/loginSuccess")
 	public RedirectView loginSucess(HttpSession httpSession){
-
 		SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
-		return new RedirectView("http://localhost:8080/main/home/"+user.getEmail());
+		BlogConfig config = new BlogConfig(user.getEmail(), user.getName() + "의 블로그", user.getName() + "의 블로그 입니다.", 
+			"http://i3a604.p.ssafy.io/images/profile/blog_basic.jpg");
+		configRepository.save(config);
+		
+		return new RedirectView("http://localhost:8080/account/setNickname/"+user.getEmail());
 	}
 }
 
