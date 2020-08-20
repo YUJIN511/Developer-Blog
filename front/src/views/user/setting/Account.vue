23228a139cff44afedb331ae849d7531bd1ab125
고유진 <smile970511@gmail.com>
2020-08-20 22:13:04 +0900

    [fix] delete social connect

--- front/src/views/user/setting/Account.vue
+++ front/src/views/user/setting/Account.vue
@@ -35,14 +35,14 @@
           />
         </button>
       </div>
-      <div class="level-icon">
+      <!-- <div class="level-icon">
         <span>연동된 SNS계정</span>
         <div class="container-btn-social">
           <button class="btn-circle btn-google"></button>
           <button class="btn-circle btn-facebook"></button>
           <button class="btn-circle btn-github"></button>
         </div>
-      </div>
+      </div> -->
       <div class="level-icon">
         <span class="span-password" @click="enablePassword()"
           >비밀번호 바꾸기</span
